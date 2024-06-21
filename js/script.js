57672ca231c38e2249899955ca0d3b3b7892342d
muhamadiqbal29 <muahamadiqbal68@gmail.com>
2024-06-21 13:33:46 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// conditional navbar scrolling
+const nav = document.getElementById('nav');
+
+window.addEventListener('scroll', function(){
+  scrollposition = window.scrollY;
+
+  if(scrollposition >= 60 ) {
+     nav.classList.add('nav-dark');
+  } else if (scrollposition <= 0) {
+    nav.classList.remove('nav-dark')
+  }
+})
